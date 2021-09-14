e96ca05734a906b5507732134a72dab168c546f9
Sayan Nandan <nandansayan@outlook.com>
2021-09-14 09:03:17 -0700

    Pull before push to avoid fetch-first errors

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,27 @@
+/*
+ * Created on Thu Sep 14 2021
+ *
+ * This file is a part of Skytable's "Perf" tool
+ * Skytable's performance tool is used to analyze the performance of the
+ * Skytable database.
+ *
+ * Copyright (c) 2021, Sayan Nandan <ohsayan@outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+*/
+
 macro_rules! err {
     ($e:expr) => {{
         log::error!("{}", $e);
@@ -70,6 +94,8 @@ macro_rules! commit {
                 $msg
             ),*
         );
+        trace!("Pulling latest ... ");
+        cmderr!("git", "pull", "--rebase");
         trace!("Publishing results ...");
         cmderr!(
             "git",
